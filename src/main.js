16bd056ab334aef58d2b0d5df3cb370ab98334dd
Tonmoy777 <tomsarker210@gmail.com>
2023-06-13 15:43:15 +0600

    Assignment 6

---
+++ src/main.js
@@ -0,0 +1,18 @@
+const search = "A+";
+
+// filter data
+const result = bloodDonor.filter(
+  (data) =>
+    data.bloodGroup === search || data.cell === search || data.age >= search
+);
+
+// final data
+const finalResult = [];
+
+//get data;
+result.forEach((items, index) => {
+  finalResult.push(items);
+});
+
+// Show console
+console.table(finalResult);
